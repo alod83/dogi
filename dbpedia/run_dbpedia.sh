2396aad16749e8c5add493034f48e6e13f095324
Angelica Lo Duca <angelica@MacBook-Pro-di-Angelica.local>
2016-06-28 16:24:38 +0200

    updated run_dbpedia

--- dbpedia/run_dbpedia.sh
+++ dbpedia/run_dbpedia.sh
@@ -26,16 +26,22 @@ fi
 echo "WELCOME TO THE DBPEDIA EXTRACTION AND FILTERING PROCEDURE"
 echo "Make sure that mysql and PHP are in your PATH"
 
+# WARNING: set mysql password
 echo "Duplicating table tabDBpedia from $old_db to $new_db"
 mysql -u root -e "USE $new_db; CREATE TABLE IF NOT EXISTS tabDBpedia LIKE $old_db.tabDBpedia;INSERT tabDBpedia SELECT * FROM $old_db.tabDBpedia;"; 
 echo "Done"
 
 # run dbpedia extractor to extract links
 echo "Extracting new URLs from DBPedia"
-php dbpedia_extract_urls.php -o $old_db -n $new_db
+php dbpedia_extract_urls.php -o $old_db -n $new_db;
 echo "Done"
 
 # run dbpedia filter to filter only to some classes
 echo "Filtering extracted URLs"
-php dbpedia_filter_classes.php -n $new_db
+php dbpedia_filter_classes.php -n $new_db;
+
+
+# Remove all unfiltered links
+# WARNING: set mysql password
+mysql -u root -e "USE $new_db; DELETE FROM tabDBpedia WHERE Filtered = 'NO';"; 
 echo "Done"
